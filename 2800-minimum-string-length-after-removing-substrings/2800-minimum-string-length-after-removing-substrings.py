3e26760eb757a124b00bf196ab87f0e1a022d5e9
Sduprey18 <135675432+Sduprey18@users.noreply.github.com>
2024-10-11 18:52:16 -0400

    Time: 49 ms (30.39%), Space: 16.6 MB (57.83%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.py
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.py
@@ -2,9 +2,28 @@ class Solution:
     def minLength(self, s: str) -> int:
         #how long is ur thing supposed to run 
         #while ur string contains an AB OR a CD in it
+        '''
         while "AB" in s or "CD" in s:
           if "AB" in s:
             s = s.replace("AB", "")
           if "CD" in s:
             s = s.replace("CD", "")
         return len(s)
+        '''
+        # stack version 
+        stack = []
+        for letter in s:
+          if stack and stack[-1] =="A":
+            if letter =="B":
+              stack.pop()
+            else:
+              stack.append(letter)
+          elif stack and stack[-1] =="C":
+            if letter =="D":
+              stack.pop()
+            else:
+              stack.append(letter)
+          else:
+            stack.append(letter)
+        return len(stack)
+          
