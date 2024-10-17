7be028cab74b562466907264dd7074227e0bbe4d
Sduprey18 <135675432+Sduprey18@users.noreply.github.com>
2024-10-17 18:48:27 -0400

    Time: 11 ms (100%), Space: 30.8 MB (91.19%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,8 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+      bag = set()
+      for num in nums:
+        if num in bag:
+          return True
+        bag.add(num)
+      return False
