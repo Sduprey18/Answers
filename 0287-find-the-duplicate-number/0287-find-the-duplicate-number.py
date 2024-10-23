803cacfc6aa4e13438c5648bb0430ee051cc1de3
Sduprey18 <135675432+Sduprey18@users.noreply.github.com>
2024-10-23 19:42:19 -0400

    Time: 30 ms (96.05%), Space: 28.5 MB (95.82%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -1,8 +1,28 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        ans = set()
-        for num in nums:
-            if num in ans:
-                return num
-            else:
-                ans.add(num) 
+        #floyd cycle detection
+        slow, fast = nums[0], nums[0]
+        #treat this as a ll, since you know that theyre numbered 1 to n
+        #use the values to tell u what "node" to go to next
+        while True:
+            #move fast pointer 2 steps
+            fast = nums[nums[fast]]
+            #then you move the slow pointer only one
+            slow = nums[slow]
+            '''
+            you found the cycle, and there is NO chance of fast going out of bounds, because
+            this question is guarnateeed to give u a cycle, since the numbers are from 1-n,
+            with the arr size being n+1
+            '''
+            if slow == fast:
+                break
+        
+        #ok, since you found the cycle, you restart again, put one of the pointers to 0 
+        #(doesnt matter which one, then return either pointer)
+
+        slow = nums[0]
+        while slow!= fast:
+            fast = nums[fast]
+            slow = nums[slow] 
+        return slow
+
