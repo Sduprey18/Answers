f87fb872c131aedfd5cdb773ec3ad587060dad5f
Sduprey18 <135675432+Sduprey18@users.noreply.github.com>
2024-11-22 13:02:41 -0500

    Time: 7 ms (48.27%), Space: 17.7 MB (98.89%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.py
@@ -0,0 +1,37 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        #first we want to create indegree
+        indegree = [0] * numCourses
+
+        adjList = defaultdict(list)
+        #making adj list
+        
+        for course, preReq in prerequisites:
+            if preReq not in adjList:
+                adjList[preReq] = []
+            adjList[preReq].append(course)
+
+            indegree[course] += 1
+
+        
+        q = []
+        for i in range(len(indegree)):
+            if indegree[i] == 0:
+                q.append(i)
+        
+        while q:
+            index= q.pop(0)
+            curr = adjList[index]
+            for child in curr:
+                indegree[child] -=1
+                if indegree[child] == 0:
+                    q.append(child)
+        
+        total = sum(indegree)
+        if total>0:
+            return False
+        return True
+
+
+
+        
