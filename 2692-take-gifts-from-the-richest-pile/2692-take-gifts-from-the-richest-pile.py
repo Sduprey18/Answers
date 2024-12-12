9a8965be0656df1f8a3fed2a507f248e63441a36
Sduprey18 <135675432+Sduprey18@users.noreply.github.com>
2024-12-11 23:37:21 -0500

    Time: 71 ms (15.84%), Space: 17.3 MB (25.1%) - LeetHub

--- 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.py
+++ 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.py
@@ -2,15 +2,9 @@ class Solution:
     def pickGifts(self, gifts: List[int], k: int) -> int:
         for i in range(k):
             _max = max(gifts)
-            
             newVal = int(sqrt(_max))
-
             index = gifts.index(_max)
-
             gifts[index] = newVal
-            
-            
-
         return sum(gifts)
 
         
