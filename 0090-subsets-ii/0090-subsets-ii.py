26612e991ab67422b82ab51167e4e11b75a481a6
Sherkeem Duprey <135675432+Sduprey18@users.noreply.github.com>
2025-09-13 16:00:46 -0400

    Time: 0 ms (100%), Space: 17.9 MB (85.13%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -1,18 +1,24 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        ans, curr = [], []
+        res = []
         nums.sort()
-        def backtrack(start):
-            print(curr)
-            if curr not in ans:
-                ans.append(curr[:])      
-            for i in range(start, len(nums)):
-            # Skip duplicates
-                if i > start and nums[i] == nums[i - 1]:
-                    continue
-                curr.append(nums[i])
-                backtrack(i + 1)
-                curr.pop()
 
-        backtrack(0)
-        return ans
+        def backtrack(path, index):
+          res.append(path[:])
+
+          for i in range(index,len(nums)):
+
+            if i>index and nums[i] == nums[i-1]:
+              continue 
+            #first choose to add
+            path.append(nums[i])
+
+            #then choose to backtrack
+            backtrack(path,i+1)
+
+            #then pop for all combos 
+            path.pop()
+        
+        backtrack([],0)
+        
+        return res
