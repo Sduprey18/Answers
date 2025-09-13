ab3fb22a23b59bd5ed11eda496d9113f081a299b
Sherkeem Duprey <135675432+Sduprey18@users.noreply.github.com>
2025-09-13 15:48:00 -0400

    Time: 0 ms (100%), Space: 17.6 MB (99.03%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,15 +1,19 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-      res = [] #holds total subset
-      subset = []#holds current subset
-      def dfs(i):
-        if i >=len(nums):#once I is over the len(nums) then you have nothing to iterate
-          res.append(subset.copy())
-          return 
-        subset.append(nums[i])#since its subset, u will have to include curr ele
-        dfs(i+1)
-        subset.pop()#since its subste, you will also have to include set, without current ele
-        dfs(i+1)
-        return
-      dfs(0)#the dfs will iterate thru itself with the dfs(i+1)
-      return res
+        solution = []
+
+        def backtrack(path, start):
+          solution.append(path[:])
+
+          for i in range(start, len(nums)):
+            #first choose
+            path.append(nums[i])
+            #then backtrack 
+            backtrack(path,i+1)
+            #then remove the curr, to get a diff combo
+            path.pop()
+        
+        backtrack([],0)
+
+        return solution
+         
