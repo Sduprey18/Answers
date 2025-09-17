9ca5bbaa4bd52c6aa5ed311d108d07dd4e55b985
Sherkeem Duprey <135675432+Sduprey18@users.noreply.github.com>
2025-09-16 21:58:48 -0400

    Time: 0 ms (100%), Space: 18 MB (26.13%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,10 +1,27 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        #bottom row you can only move right, so lets make that first
-        row =[1] * n
-        for i in range(m-1):
-          newRow =[1] * n
-          for j in range(n-2,-1,-1):
-            newRow[j] = row[j] + newRow[j + 1]
-            row[j] = newRow[j]
-        return row[0]
+      rows, cols = m, n
+      dp = []
+      for i in range(m):
+        row =[]
+        for j in range(n):
+          row.append(0)
+        dp.append(row)
+
+      dp[0][0] = 1
+
+      #fill first row
+      for i in range(1,cols):
+        dp[0][i] =  1
+      
+      #fill first col 
+      for i in range(1,rows):
+        dp[i][0] =  1
+      
+      #fill in the rest 
+      for i in range(1,rows):
+        for j in range(1,cols):
+          dp[i][j] = dp[i][j-1] + dp[i-1][j]
+      
+      return dp[rows-1][cols-1]
+        
